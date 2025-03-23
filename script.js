a1cf7ce38193bea76105e260d6758eaf04c58520
zeff2421 <191776689+zeff2421@users.noreply.github.com>
2025-03-23 16:32:47 +0200

    Create the 16x16 grid using Flexbox

---
+++ script.js
@@ -0,0 +1,18 @@
+const WIDTH = 900;
+const HEIGHT = 900;
+
+let GRID_LENGTH = 16;
+let count = GRID_LENGTH ** 2;
+
+let container = document.querySelector(".container");
+
+while (count > 0){
+    let grid = document.createElement("div");
+    let grid_height = HEIGHT / GRID_LENGTH;
+    let grid_width = WIDTH / GRID_LENGTH;
+
+    grid.style.cssText = `height: ${grid_height}px; width: ${grid_width}px; border: .1px dashed #EBECF0; box-sizing: border-box;`;
+    container.appendChild(grid);
+
+    count--;
+}

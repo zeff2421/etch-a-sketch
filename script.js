4f742562b4d60ef60231f7a80ebefdad148dc189
zeff2421 <191776689+zeff2421@users.noreply.github.com>
2025-03-30 00:51:58 +0200

    Add a button that prompt the user for input

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 const WIDTH = 800;
 const HEIGHT = 800;
 
-let GRID_LENGTH = 16;
-let count = GRID_LENGTH ** 2;
+let GRID_LENGTH;
+
 
 let container = document.querySelector(".container");
+let btn = document.querySelector("button");
 
-while (count > 0){
-    let grid = document.createElement("div");
-    let grid_height = HEIGHT / GRID_LENGTH;
-    let grid_width = WIDTH / GRID_LENGTH;
+window.addEventListener("load", () => {
+    GRID_LENGTH = parseInt(prompt("Enter the number of squares per side: "));
 
-    grid.style.cssText = `height: ${grid_height}px; width: ${grid_width}px; box-sizing: border-box;`;
-    container.appendChild(grid);
+    let count = GRID_LENGTH ** 2;
 
-    count--;
-}
+    while (count > 0){
+        let grid = document.createElement("div");
+        let grid_height = HEIGHT / GRID_LENGTH;
+        let grid_width = WIDTH / GRID_LENGTH;
 
-let squareDivs = document.querySelectorAll(".container div");
-console.log(squareDivs);
+        grid.style.cssText = `height: ${grid_height}px; width: ${grid_width}px; box-sizing: border-box;`;
+        container.appendChild(grid);
 
-squareDivs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "#808080";
-    })
-});
+        count--;
+    }
+
+    let squareDivs = document.querySelectorAll(".container div");
+    console.log(squareDivs);
+
+    squareDivs.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "#808080";
+        })
+    });
+})
+
+btn.addEventListener("click", () => {
+    window.location.reload();
+})
